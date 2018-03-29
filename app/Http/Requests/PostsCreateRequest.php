5709ba54b08072747a4f9463ae8117353e1327f2
umeshp1212 <umeshp1212@gmail.com>
2018-03-29 16:46:14 +0530

    able to create posts with dynamic categories

--- app/Http/Requests/PostsCreateRequest.php
+++ app/Http/Requests/PostsCreateRequest.php
@@ -24,10 +24,10 @@ class PostsCreateRequest extends Request
     public function rules()
     {
         return [
-//            'category_id'=>'required',
-//            'photo_id'=>'required',
-//            'title'=>'required',
-//            'body'=>'required'
+            'category_id'=>'required',
+            'photo_id'=>'required',
+            'title'=>'required',
+            'body'=>'required'
         ];
     }
 }
